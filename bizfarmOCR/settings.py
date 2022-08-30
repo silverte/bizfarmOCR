b2c0c6b072fd725d2dec0805cb294901cf1c662c
silverte <km78cho@gmail.com>
2022-08-30 10:50:30 +0900

    removed

--- bizfarmOCR/settings.py
+++ bizfarmOCR/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@jnfau#99zsougf%%rxcarktzr2o-8azx3338#+ec2b^uz@vf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -200,4 +200,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = '309.garage@gmail.com'
-EMAIL_HOST_PASSWORD = 'meplwxpkizwnitzz'
+EMAIL_HOST_PASSWORD = ''
